6e0002d9ad10c10c1b26ee537f9430656f7b679d
Robin Winter <robin.winter@bayer.com>
2018-09-07 14:03:48 +0200

    bla

--- cddd/test.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--cell_size', nargs='+', default=[128], type=int)
-    flags, unparsed = parser.parse_known_args()
-    print(flags.cell_size)
